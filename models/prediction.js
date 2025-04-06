ed1b90e7c66d153d368516ed92cb1e5ec8b6367b
banicata93 <banicata93@gmail.com>
2025-04-06 17:06:13 +0300

    Create prediction.js

---
+++ models/prediction.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const predictionSchema = new mongoose.Schema({
+    matchDate: {
+        type: Date,
+        required: true
+    },
+    leagueFlag: {
+        type: String,
+        required: true,
+        maxlength: 4
+    },
+    homeTeam: {
+        type: String,
+        required: true
+    },
+    awayTeam: {
+        type: String,
+        required: true
+    },
+    prediction: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Prediction', predictionSchema);
