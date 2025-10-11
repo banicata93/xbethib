e8336ac654ec4abe647f2b777ac4a12e66047b10
banicata93 <banicata93@gmail.com>
2025-10-11 14:02:14 +0300

    Update prediction.js

--- models/prediction.js
+++ models/prediction.js
@@ -21,16 +21,6 @@ const predictionSchema = new mongoose.Schema({
     prediction: {
         type: String,
         required: true
-    },
-    confidence: {
-        type: Number,
-        min: 0,
-        max: 100,
-        default: 70
-    },
-    source: {
-        type: String,
-        default: 'manual'
     }
 }, {
     timestamps: true
