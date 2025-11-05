474466008cf6003b195a8dd444de6fe9f2634e07
banicata93 <banicata93@gmail.com>
2025-11-05 09:09:06 +0200

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadAdminPredictions();
     
     if (predictionForm) {
-        predictionForm.addEventListener('submit', handleAddPrediction);
+        predictionForm.addEventListener('submit', window.handleAddPrediction);
     }
     
     // Event delegation for dynamically created buttons
